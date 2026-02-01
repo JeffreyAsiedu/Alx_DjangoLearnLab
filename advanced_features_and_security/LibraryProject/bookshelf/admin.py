b838e21873779c13141a6bea420a533676b0bed8
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-01 23:21:04 +0000

    Implement CustomUserAdmin for CustomUser management
    Added a custom user admin to manage CustomUser with additional fields.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,18 +1,40 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
+
+# Custom User Admin
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = (
+        "username",
+        "email",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
 
 
-# Define a custom Admin class to customize the view
+# Register CustomUser with CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Book Admin (existing)
 class BookAdmin(admin.ModelAdmin):
-    # 1. Display specific fields in the list view
     list_display = ("title", "author", "publication_year")
-
-    # 2. Add filters to the right sidebar
     list_filter = ("publication_year", "author")
-
-    # 3. Add a search box at the top
     search_fields = ("title", "author")
 
 
-# Register the model with the custom admin class
 admin.site.register(Book, BookAdmin)
