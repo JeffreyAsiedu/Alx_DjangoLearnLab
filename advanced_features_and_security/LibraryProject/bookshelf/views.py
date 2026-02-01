985aec71b2be35a23ab2bdf5a8e969f6a2f997d3
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-01 23:27:43 +0000

    Add authentication to book list view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
 
-# Create your views here.
+
+@login_required
+@permission_required("bookshelf.view_book", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
