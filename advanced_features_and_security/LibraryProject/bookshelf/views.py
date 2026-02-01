369151b439b5856e53a19cff576bd2e9b0dd18a3
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-01 23:34:29 +0000

    Import ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
+from .forms import ExampleForm 
 
 
 @login_required
