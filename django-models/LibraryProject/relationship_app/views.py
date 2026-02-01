f449be68922bd6efa9c19f2f7e5d69052e23b581
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-01 23:50:26 +0000

    Add view to list all books from bookshelf

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
+from bookshelf.models import Book  # Import Book from bookshelf app
 
+def list_books(request):
+    # Query all books
+    books = Book.objects.all()
+    
+    # Render the required template
+    return render(request, "relationship_app/list_books.html", {"books": books})
