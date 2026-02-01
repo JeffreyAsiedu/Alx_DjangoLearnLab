3844470a61e0ee82b1e0d525039b8141522e14a7
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-01 23:56:14 +0000

    Add library detail view with get_object_or_404
    Added a library detail view to fetch and display library information.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from bookshelf.models import Book  
 from .models import Library
 
+# Existing books list view
 def list_books(request):
-    # Query all books
     books = Book.objects.all()
-    
-    # Render the required template
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Required library detail view
+def library_detail(request, pk):
+    library = get_object_or_404(Library, pk=pk) 
+    return render(request, "relationship_app/library_detail.html", {"library": library})
