9049f319a3a869582462eb1d03837d25a8a5845a
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-02 00:13:45 +0000

    Update URL paths to use views module

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
+from . import views  # import the module
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # List all books
-    path("books/", list_books, name="list_books"),
+    path("books/", views.list_books, name="list_books"),
 
     # Library detail view
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # User registration
-    path("register/", register, name="register"),
+    # User registration (checker requires views.register)
+    path("register/", views.register, name="register"),
 
     # User login
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
