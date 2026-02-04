d34a9f0d3db291a562a0a58d6391239a216dacae
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-04 00:42:01 +0000

    Implement role-based access views
    Added user role checks and views for admin, librarian, and member.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
 from bookshelf.models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
 
 # Authentication imports
 from django.contrib.auth import login
@@ -39,3 +40,26 @@ def register(request):
         form = UserCreationForm()
 
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
