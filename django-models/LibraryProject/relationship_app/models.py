ccd4bde352a3036a3d7c481b123ab6c88f664684
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-04 00:51:50 +0000

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -15,6 +15,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
     def __str__(self):
         return self.title
 
@@ -69,3 +76,6 @@ class Member(models.Model):
 
     def __str__(self):
         return f"Member: {self.profile.user.username}"
+
+
+
