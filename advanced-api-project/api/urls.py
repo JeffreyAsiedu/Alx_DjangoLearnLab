9e9994aaab1377dbabf8307861407cc757e56437
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-06 22:49:26 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,8 +12,12 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 
-    # Restricted (authenticated users only)
+    # Create
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+
+    # Update (REQUIRED: contains "books/update")
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+
+    # Delete (REQUIRED: contains "books/delete")
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
