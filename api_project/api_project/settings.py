ffd00ad756cef46c0815630dfa1c0cc2269403e6
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-06 00:58:35 +0000

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -101,6 +101,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
