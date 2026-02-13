a23efb4da1cf5afb93ae6ed8c0ed2ca6121c8bce
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-13 00:26:01 +0000

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',
+        'USER': 'mydb',
+        'PASSWORD': 'Joel@20199',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
