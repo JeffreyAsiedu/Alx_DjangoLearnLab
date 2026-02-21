e26b18e80a1c6c9e4e999e2f6f6bb0a7697f7f3d
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2026-02-21 02:47:44 +0000

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model()
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
